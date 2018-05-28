30603295d5931c185050a0e2bb01d9d6fed900ca
Yannick Decock <decock.yannick@hotmail.com>
2018-05-28 10:27:56 +0200

    dit is een test commit om naam en email te testen

---
+++ CallbackFunction1/src/CallbackFunction.java
@@ -0,0 +1,13 @@
+public class CallbackFunction {
+    public static void main(String[] args) {
+
+        public void CallbackFunction(String name){
+            System.out.println("Hello " +name);
+
+        }
+
+        public void Greet(CallbackFunction){ //dit is een testje
+
+        }
+    }
+}
